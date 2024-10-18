952a6401210700d4bb1706440ac849783efeaeac
Bei@ubuntu <170186969@qq.com>
2024-10-18 21:20:20 +0800

    "修改完成OpenposeMsg 缺少Ros节点编写"

--- ros_openpose/scripts/OpenposeMsg.py
+++ ros_openpose/scripts/OpenposeMsg.py
@@ -4,16 +4,9 @@ import os
 import math
 import sys
 import cv2
-from torch import nn, optim
+from torch import nn
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
-
-#站立、躺、坐、平坐、行走、蹲起、俯卧撑、摔倒、
-#倚靠墙壁、吸烟、打电话、挥手、举手、挥双手
-pos = ["站立(正面）", "躺（正面）", "坐（正面）", "平坐", "行走（正面）", "蹲起（正面）",
-       "俯卧撑", "摔倒（正面）", "倚靠墙壁", "吸烟", "打电话", "挥手", "举手",
-       "挥双手", "站立（侧面）", "躺下（侧面）", "坐（侧面）", "行走（侧面）", "蹲起（侧面）",
-       "俯卧撑（侧面）", "摔倒（侧面）", "打电话（侧面）", "吸烟（侧面）", "举手（侧面）"]
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -37,6 +30,13 @@ opWrapper = op.WrapperPython()
 opWrapper.configure(params)
 opWrapper.start()
 
+#站立、躺、坐、平坐、行走、蹲起、俯卧撑、摔倒、
+#倚靠墙壁、吸烟、打电话、挥手、举手、挥双手
+pos = ["站立(正面）", "躺（正面）", "坐（正面）", "平坐", "行走（正面）", "蹲起（正面）",
+       "俯卧撑", "摔倒（正面）", "倚靠墙壁", "吸烟", "打电话", "挥手", "举手",
+       "挥双手", "站立（侧面）", "躺下（侧面）", "坐（侧面）", "行走（侧面）", "蹲起（侧面）",
+       "俯卧撑（侧面）", "摔倒（侧面）", "打电话（侧面）", "吸烟（侧面）", "举手（侧面）"]
+
 class Classification(nn.Module):
     def __init__(self, in_dim, n_hidden_1, n_hidden_2, n_hidden_3, out_dim):
         super(Classification, self).__init__()
@@ -55,8 +55,60 @@ class Classification(nn.Module):
         x = self.layer4(x)
 
         return x
+    
+class Video:
+    def __init__(self, capture):
+        self.capture = capture
+        self.currentFrame = None
+        self.previousFrame = None
 
-def pointDistance(self, keyPoint):
+    def captureFrame(self):
+        """
+        capture frame and return captured frame
+        """
+        ret, readFrame = self.capture.read()
+        return readFrame
+
+    def captureNextFrame(self):
+        """
+        capture frame and reverse RBG BGR and return opencv image
+        """
+        ret, readFrame = self.capture.read()
+        if ret:
+            self.currentFrame = cv2.cvtColor(readFrame, cv2.COLOR_BGR2RGB)
+
+    def convertFrame(self):
+        # converts frame to format suitable for QtGui
+        try:
+            height, width = self.currentFrame.shape[:2]
+
+            self.previousFrame = self.currentFrame
+            #return img
+        except cv2.Error:
+            return None
+        
+
+
+def predict_result(datas=None):
+    """
+    :param datas: list
+    :return: int
+    """
+    if datas is None:
+        datas = []
+    model = Classification(30, 200, 300, 100, 24)
+    model.load_state_dict(torch.load(
+        "../model_pth/ros_pose.pth", map_location='cpu'))
+    predict = model(Variable(torch.Tensor([datas]).float())).detach().cpu().numpy().tolist()[0]
+    predict = predict.index(max(predict))
+
+    return predict
+
+class MainOpenpose:
+    def __init__(self):
+          self.video = Video(cv2.VideoCapture(0))
+
+    def pointDistance(self, keyPoint):
         """
         :param keyPoint:
         :return:list
@@ -96,7 +148,7 @@ def pointDistance(self, keyPoint):
         return [distance0, distance1, distance2, distance3, distance4, distance5, distance6, distance7,
                 distance8, distance9, distance10, distance11, distance12, distance13, distance14]
 
-def pointAngle(self, keyPoint):
+    def pointAngle(self, keyPoint):
         angle0 = self.myAngle(keyPoint[2], keyPoint[3], keyPoint[4])
         angle1 = self.myAngle(keyPoint[5], keyPoint[6], keyPoint[7])
         angle2 = self.myAngle(keyPoint[9], keyPoint[10], keyPoint[11])
@@ -116,7 +168,7 @@ def pointAngle(self, keyPoint):
         return [angle0, angle1, angle2, angle3, angle4, angle5, angle6, angle7,
                 angle8, angle9, angle10, angle11, angle12, angle13, angle14]
 
-def myAngle(self, A, B, C):
+    def myAngle(self, A, B, C):
         c = math.sqrt((A[0] - B[0]) ** 2 + (A[1] - B[1]) ** 2)
         a = math.sqrt((B[0] - C[0]) ** 2 + (B[1] - C[1]) ** 2)
         b = math.sqrt((A[0] - C[0]) ** 2 + (A[1] - C[1]) ** 2)
@@ -124,25 +176,9 @@ def myAngle(self, A, B, C):
             return (a ** 2 + c ** 2 - b ** 2) / (2 * a * c)
         return 0
 
-
-def predict_result(datas=None):
-    """
-    :param datas: list
-    :return: int
-    """
-    if datas is None:
-        datas = []
-    model = Classification(30, 200, 300, 100, 24)
-    model.load_state_dict(torch.load(
-        "../model_pth/ros_pose.pth", map_location='cpu'))
-    predict = model(Variable(torch.Tensor([datas]).float())).detach().cpu().numpy().tolist()[0]
-    predict = predict.index(max(predict))
-
-    return predict
-
-def showCapture(self):
-    try:
-
+    def showCapture(self):
+        try:
+            #frame = self.video.captureFrame()
             frame = self.video.captureFrame()
             datum = op.Datum()
             datum.cvInputData = frame
@@ -150,26 +186,27 @@ def showCapture(self):
             resPic = datum.cvOutputData
 
             pic = cv2.cvtColor(resPic, cv2.COLOR_BGR2RGB)
-
+            #cv2.imshow("pic",pic)
             if datum.poseKeypoints is None:
                 pass
             else:
+                print(f"Pose Keypoints: {datum.poseKeypoints}")
                 keyPoints = datum.poseKeypoints.tolist()
-                self.label_4.setText(pos[predict_result(pointDistance(keyPoints[0]) +
-                                                    pointAngle(keyPoints[0]))])
-                print(pos[predict_result(pointDistance(keyPoints[0]) +
-                                                    pointAngle(keyPoints[0]))])
-#             keyPoints = datum.poseKeypoints.tolist()
-#             self.label_4.setText(pos[predict_result(pointDistance(keyPoints[0]) +
-#                                                     pointAngle(keyPoints[0]))])
+                #self.label_4.setText(pos[predict_result(pointDistance(keyPoints[0]) +
+                #                                    pointAngle(keyPoints[0]))])
+                print(pos[predict_result(self.pointDistance(keyPoints[0]) + self.pointAngle(keyPoints[0]))])
 
-    except TypeError:
-            print("No frame")
+        except TypeError as e:
+            print(f"TypeError occurred: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
-    data = []
-    # print(predict_result(data))
-    predict_result(data)
-
+    #data = []
+    #predict_result(data)
+    #print(predict_result(data))
+    myOpenpose = MainOpenpose()
+    while True:
+        myOpenpose.showCapture()
 
 
